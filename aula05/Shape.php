2bd96905c7d6966515a3f7cf97e3619f34955914
RodrigoJimmy <rodrigodelimavieira@gmail.com>
2013-03-16 14:52:23 -0400

    Classes abstratas

---
+++ aula05/Shape.php
@@ -0,0 +1,15 @@
+<?php
+
+abstract class Shape {
+    
+    protected $x, $y;
+    
+    function setCenter($x, $y)
+    {
+        $this->x = $x;
+        $this->y = $y;
+    }
+    
+    abstract function draw();
+    
+}
